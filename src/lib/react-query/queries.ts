597e0175609e5fa4e3c1dcc18296f3f114fe821c
Andy yang <68847099+IlIIIIIIlI@users.noreply.github.com>
2024-03-05 23:34:25 +1100

    Update queries.ts

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -55,23 +55,23 @@ export const useSignOutAccount = () => {
 // POST QUERIES
 // ============================================================
 
-export const useGetPosts = () => {
-  return useInfiniteQuery({
-    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts as any,
-    getNextPageParam: (lastPage: any) => {
-      // If there's no data, there are no more pages.
-      if (lastPage && lastPage.documents.length === 0) {
-        return null;
-      }
+   export const useGetPosts = () => {
+      return useInfiniteQuery({
+         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+        queryFn: getInfinitePosts,
+      getNextPageParam: (lastPage) => {
+        // If there's no data, there are no more pages.
+        if (lastPage && lastPage.documents.length === 0) {
+          return null;
+           }
+    // Use the $id of the last document as the cursor.
+  
+  const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
 
-      // Use the $id of the last document as the cursor.
-      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-      return lastId;
-    },
-    initialPageParam: null,
+    return lastId;
+   },
   });
-};
+  };
 
 export const useSearchPosts = (searchTerm: string) => {
   return useQuery({
